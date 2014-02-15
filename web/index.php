96f103aac142e9f57702bb0d6c6de6ee3d407fe8
Osman Ungur <osmanungur@gmail.com>
2014-02-15 16:56:07 +0200

    Implemented /users endpoints

--- web/index.php
+++ web/index.php
@@ -7,7 +7,102 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-/*
+$app->before(
+    function(Request $request) use ($app) {
+        if ($request->headers->get('api-username') != 'TestUser') {
+            return $app->json(
+                array(
+                    'message' => "You're not allowed to see this page",
+                    'code' => 1000
+                ),
+                401
+            );
+        }
+    }
+);
+
+$app->delete('users/{id}', function($id) use ($app) {
+    $app['db']->delete(
+        'users',
+        array(
+            'id' => $id
+        )
+    );
+
+    return $app->json(
+        array(
+            'message' => 'User deleted succesfully!'
+        ),
+        204
+    );
+});
+
+$app->put('users/{id}', function($id) use ($app) {
+    $app['db']->update(
+        'users',
+        array(
+            'name' => $app['request']->request->get('name'),
+            'surname' => $app['request']->request->get('surname'),
+            'email' => $app['request']->request->get('email'),
+            'is_active' => $app['request']->request->get('is_active'),
+        ),
+        array(
+            'id' => $id
+        )
+    );
+
+    return $app->json(
+        array(
+            'message' => 'User updated succesfully!'
+        ),
+        204
+    );
+});
+
+
+$app->post('users', function() use ($app) {
+    $app['db']->insert(
+        'users',
+        array(
+            'name' => $app['request']->request->get('name'),
+            'surname' => $app['request']->request->get('surname'),
+            'email' => $app['request']->request->get('email'),
+            'is_active' => 0
+        )
+    );
+
+    return $app->json(
+        array(
+            'message' => 'User created succesfully!'
+        ),
+        201
+    );
+});
+
+$app->get(
+    'users/{id}',
+    function($id) use ($app) {
+        return $app->json(
+            $app['db']->executeQuery(
+                'SELECT * FROM users WHERE is_active = 1 AND id = :user_id',
+                array('user_id' => $id)
+            )->fetch()
+        );
+    }
+);
+
+
+$app->get(
+    'users',
+    function() use ($app) {
+        return $app->json(
+            $app['db']->executeQuery(
+                'SELECT * FROM users where is_active = 1'
+            )->fetchAll()
+        );
+    }
+);
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'    => 'pdo_mysql',
@@ -18,7 +113,6 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'charset'   => 'utf8'
     ),
 ));
-*/
 
 $app->before(function (Request $request) {
     $data = json_decode($request->getContent(), true);
@@ -37,5 +131,6 @@ $app->error(function (\Exception $e, $code) use ($app) {
         )
     );
 });
+
 
 $app->run();
